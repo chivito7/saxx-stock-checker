c632b549ef544ce0486d612b3189435e6a3171e7
Daniel Barbosa <124114502+chivito7@users.noreply.github.com>
2025-05-31 15:56:51 -0700

    Update saxx_checker.py

--- saxx_checker.py
+++ saxx_checker.py
@@ -1,23 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import smtplib
 from email.message import EmailMessage
 import os
 
 URL = 'https://www.saxxunderwear.com/products/sxpp3jb_bnw?variant=39910770573398'
 
-def send_email(stock_status):
+def send_email():
     msg = EmailMessage()
-    msg.set_content(f"🚨 SAXX item status: {stock_status}. Check here: {URL}")
-    msg['Subject'] = "SAXX Stock Alert"
-    msg['From'] = os.environ['GMAIL_ADDRESS']
-    msg['To'] = os.environ['GMAIL_ADDRESS']
+    msg.set_content("🚨 The item is IN STOCK! Check it now: https://www.saxxunderwear.com/products/sxpp3jb_bnw?variant=39910770573398")
+    msg['Subject'] = "SAXX ITEM IN STOCK"
+    msg['From'] = os.environ["GMAIL_ADDRESS"]
+    msg['To'] = os.environ["GMAIL_ADDRESS"]
 
-    gmail_app_password = os.environ['GMAIL_PASSWORD']
+    gmail_app_password = os.environ["GMAIL_PASSWORD"]
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(os.environ['GMAIL_ADDRESS'], gmail_app_password)
+        smtp.login(os.environ["GMAIL_ADDRESS"], gmail_app_password)
         smtp.send_message(msg)
 
 def check_stock():
@@ -26,12 +25,9 @@ def check_stock():
     soup = BeautifulSoup(response.text, 'html.parser')
 
     if "Sold Out" in soup.text or "Out of stock" in soup.text:
-        stock_status = "NOT in stock"
         print("Still sold out...")
     else:
-        stock_status = "IN STOCK"
-        print("🚨 It's IN STOCK! 🚨")
-
-    send_email(stock_status)
+        print("🚨 It's IN STOCK! 🚨 Sending email...")
+        send_email()
 
 check_stock()
